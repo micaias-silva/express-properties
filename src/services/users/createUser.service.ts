d1f58d2b374d80812161717f3be82220d2f04820
Micaías Silva <micaiasilvaop@gmail.com>
2022-10-23 16:17:54 -0300

    feat: tratativa de conflito de emails para serviço de criação de usuário

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -3,6 +3,7 @@ import { Users } from "../../entities/users.entity";
 import { IUser, IUserRequest, User } from "../../interfaces/users";
 import { hash } from "bcrypt";
 import { plainToClass } from "class-transformer";
+import HTTPError from "../../errors/HTTPError";
 
 const createUserService = async ({
   name,
@@ -11,6 +12,12 @@ const createUserService = async ({
   isAdm = false,
 }: IUserRequest): Promise<IUser | undefined> => {
   const userRepository = AppDataSource.getRepository(Users);
+  const emailAlreadyExists = await userRepository.findOne({ where: { email } });
+
+  if (emailAlreadyExists) {
+    throw new HTTPError(409, "Email already exists");
+  }
+
   const hashedPassword = await hash(password, 10);
 
   const newUser = userRepository.create({
@@ -22,7 +29,7 @@ const createUserService = async ({
 
   await userRepository.save(newUser);
 
-  return plainToClass(User, newUser, {excludeExtraneousValues: true});
+  return plainToClass(User, newUser, { excludeExtraneousValues: true });
 };
 
 export default createUserService;
