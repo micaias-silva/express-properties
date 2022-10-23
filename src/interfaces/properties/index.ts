fbbb9534460946ece5b8f5c7992e226bab4b79ab
Micaías Silva <micaiasilvaop@gmail.com>
2022-10-23 16:22:31 -0300

    feat: sintaxe do prettier code

--- src/interfaces/properties/index.ts
+++ src/interfaces/properties/index.ts
@@ -1,14 +1,14 @@
 export interface IAddressRequest {
-    district: string
-    zipCode: string
-    number?: string
-    city: string
-    state: string
+  district: string;
+  zipCode: string;
+  number?: string;
+  city: string;
+  state: string;
 }
 
 export interface IPropertyRequest {
-    value: number
-    size: number
-    address: IAddressRequest
-    categoryId: string
+  value: number;
+  size: number;
+  address: IAddressRequest;
+  categoryId: string;
 }
