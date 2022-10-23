ddfa24bc640b1892ce13f4d7f3e9cdff8cf7ab54
Micaías Silva <micaiasilvaop@gmail.com>
2022-10-23 16:21:50 -0300

    feat: classe para modelagem da requisição de um usuário via class-transformer

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,27 +1,48 @@
+import { Expose } from "class-transformer";
+
 export interface IUserRequest {
-    name: string
-    email: string
-    password: string
-    isAdm: boolean
+  name: string;
+  email: string;
+  password: string;
+  isAdm: boolean;
 }
 
 export interface IUser {
-    id: string
-    name: string
-    email: string
-    isAdm: boolean
-    createdAt: Date
-    updatedAt: Date
+  id: string;
+  name: string;
+  email: string;
+  isAdm: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  isActive: boolean;
 }
 
+export class User {
+  @Expose()
+  id: string;
+  @Expose()
+  name: string;
+  @Expose()
+  email: string;
+  @Expose()
+  isActive: boolean;
+  @Expose()
+  isAdm: boolean;
+  @Expose()
+  createdAt: Date;
+  @Expose()
+  updatedAt: Date;
+  @Expose()
+  deletedAt: Date;
+}
 
 export interface IUserLogin {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 export interface IUserUpdate {
-    name?: string
-    email?: string
-    password?: string
+  name?: string;
+  email?: string;
+  password?: string;
 }
