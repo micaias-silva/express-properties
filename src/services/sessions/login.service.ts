0a8c00e228854f455f4020b70785cf2fcb9d0ad4
Micaías Silva <micaiasilvaop@gmail.com>
2022-11-03 11:15:30 -0300

    Fix: Erro para usuário inativo

--- src/services/sessions/login.service.ts
+++ src/services/sessions/login.service.ts
@@ -4,6 +4,7 @@ import { IUserLogin } from "../../interfaces/users";
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 import { compareSync } from "bcrypt";
+import HTTPError from "../../errors/HTTPError";
 
 const loginService = async ({ email, password }: IUserLogin) => {
   const userRepository = AppDataSource.getRepository(Users);
@@ -12,12 +13,16 @@ const loginService = async ({ email, password }: IUserLogin) => {
   const user = await userRepository.findOne({ where: { email: email } });
 
   if (!user) {
-    throw new Error("Invalid email/password entry");
+    throw new HTTPError(403, "Invalid email/password entry");
   }
 
   const passwordMatch = compareSync(password, user.password);
   if (!passwordMatch) {
-    throw new Error("Invalid email/password entry");
+    throw new HTTPError(403, "Invalid email/password entry");
+  }
+
+  if (!user.isActive) {
+    throw new HTTPError(400, "Cannot login with a deleted user");
   }
 
   const token = jwt.sign({ id: user.id, isAdm: user.isAdm }, SECRET, {
